308dab03b0fdf56c345a92cd49fe73f24ebb8828
Jen Hail <jen@mightily.com>
2019-11-07 19:30:53 +0000

    add code in files types: .css,.map,.js,.scss,.php remove code in files types: .css,.map,.js,.scss,.php

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
 console.log("kfkib")
 console.log("lidbs")
-console.log("kxnek")
+console.log("lnvvn")
--- content.php
+++ content.php
@@ -1,5 +1,5 @@
 <?php
         echo "wunwh";
         echo "xxpcn";
-        echo "pvhos";
+        echo "vclwr";
 ?>
