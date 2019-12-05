7b6cc8af9216f9ca41b0e10013b14781cd5ce72c
Jen Hail <jen@mightily.com>
2019-12-05 22:30:38 +0000

    add code in files types: .js,.php remove code in files types: .js,.php

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 console.log("kfkib")
 console.log("lidbs")
 console.log("xcmqh")
-console.log("ewber")
+console.log("jgnag")
--- content.php
+++ content.php
@@ -4,5 +4,5 @@
         echo "mabff";
         echo "hyteh";
         echo "yyhsj";
-        echo "nifup";
+        echo "phkfi";
 ?>
